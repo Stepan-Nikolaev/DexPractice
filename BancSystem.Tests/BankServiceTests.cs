6751f8ceeb5b53fa835ad98ffe514789a6c76184
Stepan-Nikolaev <me.ctenah@gmail.com>
2021-08-24 03:52:58 +0300

    Initial commit

---
+++ BancSystem.Tests/BankServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using BancSystem.Models;
+using BancSystem.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BancSystem.Tests
+{
+    public class BankServiceTests
+    {
+        [Fact]
+        public void AddAndFindClient()
+        {
+            //Arrange
+            Client client = new Client() 
+            { Name = "Василий", Surname = "Бронштейн", Patronymic = "Иорданович", Age = 54, PassportID = 4667 };
+            Bank bank = new Bank();
+
+            //Act
+            bank.Add<Client>(client);
+            var result = bank.FindClient(client);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(client.Name, result.Name);
+            Assert.Equal(client.Surname, result.Surname);
+            Assert.Equal(client.Patronymic, result.Patronymic);
+            Assert.Equal(client.Age, result.Age);
+            Assert.Equal(client.PassportID, result.PassportID);
+        }
+    }
+}
---
+++ BancSystem.Tests/ExchangeTests.cs
@@ -0,0 +1,28 @@
+﻿using BancSystem.Currencies;
+using BancSystem.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BancSystem.Tests
+{
+    public class ExchangeTests
+    {
+        [Fact]
+        public void Exchange_100_Rub_In_Euro_1Point18()
+        {
+            //Arrange
+            Exchange exchange = new Exchange();
+            CurrencyType firstCurrency = new Ruble();
+            CurrencyType secondCurrency = new Euro();
+
+            //Act
+            var result = exchange.ConverterCurrency<CurrencyType>(100, firstCurrency, secondCurrency);
+
+            //Assert
+            Assert.Equal(1.18, Math.Round(result, 2));
+            Assert.NotEqual(1, result);
+        }
+    }
+}
